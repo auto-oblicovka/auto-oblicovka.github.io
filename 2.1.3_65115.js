b7451e835da5f4819d0c8f5a8f0e9ffdf8b97897
auto-oblicovka <61787652+auto-oblicovka@users.noreply.github.com>
2020-03-04 19:54:11 +0500

    Add files via upload

---
+++ 2.1.3_65115.js
@@ -0,0 +1,21 @@
+let lookpictor = document.getElementById('see');
+
+function KAMAZ_bamper65115restayl() {
+  lookpictor.innerHTML = `<img class="prevyu" src="Tovar/KAMAZ/Bamper/Bamper65115restayl/KAMAZ_bamper65115restayl.jpeg">`;
+}
+
+function img1() {
+  lookpictor.innerHTML = `<img class="prevyu" src="Tovar/KAMAZ/Bamper/Bamper65115restayl/img1.jpeg">`;
+}
+
+function img2() {
+  lookpictor.innerHTML = `<img class="prevyu" src="Tovar/KAMAZ/Bamper/Bamper65115restayl/img2.jpeg">`;
+}
+
+function img3() {
+  lookpictor.innerHTML = `<img class="prevyu" src="Tovar/KAMAZ/Bamper/Bamper65115restayl/img3.jpeg">`;
+}
+
+function img4() {
+  lookpictor.innerHTML = `<img class="prevyu" src="Tovar/KAMAZ/Bamper/Bamper65115restayl/img4.jpeg">`;
+}
