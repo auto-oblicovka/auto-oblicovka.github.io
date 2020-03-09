08f876582f767912ba9528c81c3293cff70eb475
auto-oblicovka <61787652+auto-oblicovka@users.noreply.github.com>
2020-03-09 18:45:25 +0500

    Add files via upload

--- js/@media.js
+++ js/@media.js
@@ -1,23 +1,25 @@
-let spisok = document.getElementById('buttom');
+let fonSitBar = document.createElement('div');
+let spisok = document.createElement('div');
+
+buttom = document.getElementById('buttom');
+
+fonSitBar.className = "siteBarFon";
+
+spisok.className = "spisok";
+spisok.innerHTML = `<ul class="menu_ul_media">
+   <li><a class="site_bar" href="1.maz_page.html"><h2>МАЗ</h2></a></li>
+   <li><a class="site_bar" href="2.0kamaz_page.html"><h2>КАМАЗ</h2></a></li>
+   <li><a class="site_bar" href="3.gaz_page.html"><h2>ГАЗ</h2></a></li>
+   <li><a class="site_bar" href="4.zil_page.html"><h2>ЗиЛ</h2></a></li>
+   <li><a class="site_bar" href="5.mtz_page.html"><h2>МТЗ</h2></a></li>
+   <li><a class="site_bar" href="6.ymaha_page.html"><h2>YMAHA</h2></a></li>
+   <li><a class="site_bar" href="7.jcb_page.html"><h2>JCB</h2></a></li>
+   <li><a class="site_bar" href="8.infiniti_page.html"><h2>INFINITI</h2></a></li>
+</ul>`;
+
 
 function menu() {
-  spisok.innerHTML = `<div class="buttom-menu-zg"  onclick="close_menu()">
-    <p>МЕНЮ</p>
-  </div>
-  <ul class="menu_ul">
-     <li><a class="site_bar" href="1.maz_page.html"><h2>МАЗ</h2></a></li>
-     <li><a class="site_bar" href="2.0kamaz_page.html"><h2>КАМАЗ</h2></a></li>
-     <li><a class="site_bar" href="3.gaz_page.html"><h2>ГАЗ</h2></a></li>
-     <li><a class="site_bar" href="4.zil_page.html"><h2>ЗиЛ</h2></a></li>
-     <li><a class="site_bar" href="5.mtz_page.html"><h2>МТЗ</h2></a></li>
-     <li><a class="site_bar" href="6.ymaha_page.html"><h2>YMAHA</h2></a></li>
-     <li><a class="site_bar" href="7.jcb_page.html"><h2>JCB</h2></a></li>
-     <li><a class="site_bar" href="8.infiniti_page.html"><h2>INFINITI</h2></a></li>
-  </ul>`;
-}
+  buttom.append(fonSitBar);
+  fonSitBar.append(spisok);
 
-function close_menu() {
-  spisok.innerHTML = `<div class="buttom-menu"  onclick="menu()">
-    <p>МЕНЮ</p>
-  </div>`;
 }
