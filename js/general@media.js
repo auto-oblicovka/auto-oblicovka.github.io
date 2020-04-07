73d65f68c8f652050516cbe71a99e5a785e77c64
auto-oblicovka <61787652+auto-oblicovka@users.noreply.github.com>
2020-04-07 13:06:05 +0500

    Add files via upload

--- js/general@media.js
+++ js/general@media.js
@@ -1,5 +1,6 @@
+// Появление сайт-бара
 let buttomoff = document.getElementById('buttom-off');
-let buttomon  = document.getElementById('buttom-on');
+    buttomon  = document.getElementById('buttom-on');
     sitbar = document.getElementById('fonSitBar');
 
 
@@ -13,4 +14,40 @@ function closeMenu() {
   buttomoff.style.display = "flex";
   buttomon.style.display = "none";
   sitbar.className = "siteBarFonNoViev";
+}
+
+// Выпадение дроп-меню КАМАЗ
+let dropKAMAZ = document.getElementById('drop_kamaz');
+    icon_drop_Kamaz_close = document.getElementById('icon_drop_kamaz_close');
+    icon_drop_kamaz_open = document.getElementById('icon_drop_kamaz_open');
+
+function dropMenuKAMAZ() {
+  icon_drop_Kamaz_close.style.display = "none";
+  icon_drop_kamaz_open.style.display = "block";
+  dropKAMAZ.style.display = "flex";
+  closeMenuMAZ()
+}
+
+function closeMenuKAMAZ() {
+  icon_drop_Kamaz_close.style.display = "block";
+  icon_drop_kamaz_open.style.display = "none";
+  dropKAMAZ.style.display = "none";
+}
+
+// Выпадение дроп-меню МАЗ
+let dropMAZ = document.getElementById('drop_maz');
+    icon_drop_maz_close = document.getElementById('icon_drop_maz_close');
+    icon_drop_maz_open = document.getElementById('icon_drop_maz_open');
+
+function dropMenuMAZ() {
+  icon_drop_maz_close.style.display = "none";
+  icon_drop_maz_open.style.display = "block";
+  dropMAZ.style.display = "flex";
+  closeMenuKAMAZ()
+}
+
+function closeMenuMAZ() {
+  icon_drop_maz_close.style.display = "block";
+  icon_drop_maz_open.style.display = "none";
+  dropMAZ.style.display = "none";
 }
