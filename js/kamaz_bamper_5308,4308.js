e3d1f3251b547b80d4f0ad7b15d4bfbc1b92f269
auto-oblicovka <61787652+auto-oblicovka@users.noreply.github.com>
2020-04-16 19:48:04 +0500

    Add files via upload

--- js/kamaz_bamper_5308,4308.js
+++ js/kamaz_bamper_5308,4308.js
@@ -1,16 +1,31 @@
 let gallary = [];
     i = 0;
     j = 1;
+    closeGallary = document.getElementById('close_gallary')
 do {
   gallary[i] = document.getElementById('gallary_img' + j);
   i = i+1;
   j = j+1;
 } while (gallary[i-1] != undefined);
 
-function see() {
-  let fonGallary = document.createElement('div');
+function close_gallary() {
+  closeGallary.style.display = "none";
+}
 
-  fonGallary.className = "fon_gallary";
-  document.body.append(fonGallary);
-  document.fonGallary.append(gallary[0]);
+function see1() {
+  let see_table = document.getElementById('see_pictor');
+  see_img = gallary[0];
+  see_img.style.width = "100%";
+  closeGallary.style.display = "flex";
+  see_table.append(see_img);
+
+}
+
+function see2() {
+  let see_table = document.getElementById('see_pictor');
+  see_img = gallary[1];
+  see_img.style.width = "100%";
+  closeGallary.style.display = "flex";
+  see_table.append(see_img);
+
 }
