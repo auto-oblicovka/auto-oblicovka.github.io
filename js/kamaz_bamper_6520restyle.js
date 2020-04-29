f1b4284578b9b1220a6e002004e0a5f1a9ac1fc7
auto-oblicovka <61787652+auto-oblicovka@users.noreply.github.com>
2020-04-29 14:46:45 +0500

    Add files via upload

--- js/kamaz_bamper_6520restyle.js
+++ js/kamaz_bamper_6520restyle.js
@@ -3,19 +3,11 @@ let gallary = [];
     closeGallary = document.getElementById('close_gallary');
     see_table = document.getElementById('see_pictor');
 
-gallary[0] = `<img class="foto" src="Tovar/KAMAZ/Bamper/Bamper5308,4308/KAMAZ_bamper5308,4308.jpeg">`;
-gallary[1] = `<img class="foto" src="Tovar/KAMAZ/Bamper/Bamper5308,4308/img1.jpeg">`;
-gallary[2] = `<img class="foto" src="Tovar/KAMAZ/Bamper/Bamper5308,4308/img2.jpeg">`;
-gallary[3] = `<img class="foto" src="Tovar/KAMAZ/Bamper/Bamper5308,4308/img3.jpeg">`;
-gallary[4] = `<img class="foto" src="Tovar/KAMAZ/Bamper/Bamper5308,4308/img4.jpeg">`;
-gallary[5] = `<img class="foto" src="Tovar/KAMAZ/Bamper/Bamper5308,4308/img5.jpeg">`;
-gallary[6] = `<img class="foto" src="Tovar/KAMAZ/Bamper/Bamper5308,4308/img6.jpeg">`;
-gallary[7] = `<img class="foto" src="Tovar/KAMAZ/Bamper/Bamper5308,4308/img7.jpeg">`;
-gallary[8] = `<img class="foto" src="Tovar/KAMAZ/Bamper/Bamper5308,4308/img8.jpeg">`;
-gallary[9] = `<img class="foto" src="Tovar/KAMAZ/Bamper/Bamper5308,4308/img9.jpeg">`;
-gallary[10] = `<img class="foto" src="Tovar/KAMAZ/Bamper/Bamper5308,4308/img10.jpeg">`;
-gallary[11] = `<img class="foto" src="Tovar/KAMAZ/Bamper/Bamper5308,4308/img11.jpeg">`;
-gallary[12] = `<img class="foto" src="Tovar/KAMAZ/Bamper/Bamper5308,4308/img12.jpeg">`;
+gallary[0] = `<img class="foto" src="Tovar/KAMAZ/Bamper/Bamper6520restayl/KAMAZ_bamper6520RESTAYL.jpeg">`;
+gallary[1] = `<img class="foto" src="Tovar/KAMAZ/Bamper/Bamper6520restayl/img1.jpeg">`;
+gallary[2] = `<img class="foto" src="Tovar/KAMAZ/Bamper/Bamper6520restayl/img2.jpeg">`;
+gallary[3] = `<img class="foto" src="Tovar/KAMAZ/Bamper/Bamper6520restayl/img3.jpeg">`;
+
 
 function close_gallary() {
   closeGallary.style.display = "none";
@@ -143,7 +135,7 @@ function next() {
   i = i + 1;
   see_img = gallary[i];
 
-  if (i<=12) {
+  if (i<=3) {
     see(see_img);
     closeGallary.style.display = "flex";
   } else {
@@ -162,7 +154,7 @@ function back() {
     see(see_img);
     closeGallary.style.display = "flex";
   } else {
-    i = 12;
+    i = 3;
     see_img = gallary[i];
     see(see_img);
     closeGallary.style.display = "flex";
