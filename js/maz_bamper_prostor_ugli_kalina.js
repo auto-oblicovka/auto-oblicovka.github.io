91d736b36485c4ef2a38a239cbee4bdf67d9e072
auto-oblicovka <61787652+auto-oblicovka@users.noreply.github.com>
2020-06-13 11:39:57 +0500

    Add files via upload

---
+++ js/maz_bamper_prostor_ugli_kalina.js
@@ -0,0 +1,165 @@
+let gallary = [];
+    i = 0;
+    closeGallary = document.getElementById('close_gallary');
+    see_table = document.getElementById('see_pictor');
+
+gallary[0] = `<img class="foto" src="Tovar/MAZ/Bamper/New_Prostor_ugli_kalina/prev_oprava_kalina.jpeg">`;
+gallary[1] = `<img class="foto" src="Tovar/MAZ/Bamper/New_Prostor_ugli_kalina/img1.jpeg">`;
+gallary[2] = `<img class="foto" src="Tovar/MAZ/Bamper/New_Prostor_ugli_kalina/img2.jpeg">`;
+gallary[3] = `<img class="foto" src="Tovar/MAZ/Bamper/New_Prostor_ugli_kalina/img3.jpeg">`;
+gallary[4] = `<img class="foto" src="Tovar/MAZ/Bamper/New_Prostor_ugli_kalina/img4.jpeg">`;
+gallary[5] = `<img class="foto" src="Tovar/MAZ/Bamper/New_Prostor_ugli_kalina/img5.jpeg">`;
+gallary[6] = `<img class="foto" src="Tovar/MAZ/Bamper/New_Prostor_ugli_kalina/img6.jpeg">`;
+
+
+function close_gallary() {
+  closeGallary.style.display = "none";
+  see_table.innerHTML = undefined;
+}
+
+function see(see_img) {
+  see_table.innerHTML = see_img;
+  closeGallary.style.display = "flex";
+}
+
+function see1() {
+  i = 0;
+  see_img = gallary[i];
+  see(see_img);
+  closeGallary.style.display = "flex";
+  return i;
+
+
+}
+
+function see2() {
+  i = 1;
+  see_img = gallary[i];
+  see(see_img);
+  closeGallary.style.display = "flex";
+  return i;
+
+}
+
+function see3() {
+  i = 2;
+  see_img = gallary[i];
+  see(see_img);
+  closeGallary.style.display = "flex";
+  return i;
+
+}
+
+function see4() {
+  i = 3;
+  see_img = gallary[i];
+  see(see_img);
+  closeGallary.style.display = "flex";
+  return i;
+
+}
+
+function see5() {
+  i = 4;
+  see_img = gallary[i];
+  see(see_img);
+  closeGallary.style.display = "flex";
+  return i;
+
+}
+
+function see6() {
+  i = 5;
+  see_img = gallary[i];
+  see(see_img);
+  closeGallary.style.display = "flex";
+  return i;
+
+}
+
+function see7() {
+  i = 6;
+  see_img = gallary[i];
+  see(see_img);
+  closeGallary.style.display = "flex";
+  return i;
+}
+
+function see8() {
+  i = 7;
+  see_img = gallary[i];
+  see(see_img);
+  closeGallary.style.display = "flex";
+  return i;
+}
+
+function see9() {
+  i = 8;
+  see_img = gallary[i];
+  see(see_img);
+  closeGallary.style.display = "flex";
+  return i;
+}
+function see10() {
+  i = 9;
+  see_img = gallary[i];
+  see(see_img);
+  closeGallary.style.display = "flex";
+  return i;
+}
+
+function see11() {
+  i = 10;
+  see_img = gallary[i];
+  see(see_img);
+  closeGallary.style.display = "flex";
+  return i;
+}
+
+function see12() {
+  i = 11;
+  see_img = gallary[i];
+  see(see_img);
+  closeGallary.style.display = "flex";
+  return i;
+}
+
+function see13() {
+  i = 12;
+  see_img = gallary[i];
+  see(see_img);
+  closeGallary.style.display = "flex";
+  return i;
+
+}
+
+
+function next() {
+  i = i + 1;
+  see_img = gallary[i];
+
+  if (i<=6) {
+    see(see_img);
+    closeGallary.style.display = "flex";
+  } else {
+    i = 0;
+    see_img = gallary[i];
+    see(see_img);
+    closeGallary.style.display = "flex";
+  }
+}
+
+function back() {
+  i = i - 1;
+  see_img = gallary[i];
+
+  if (i>=0) {
+    see(see_img);
+    closeGallary.style.display = "flex";
+  } else {
+    i = 6;
+    see_img = gallary[i];
+    see(see_img);
+    closeGallary.style.display = "flex";
+  }
+}
